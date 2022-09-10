1fc7fb0e4941811ffe4d318fa9cef614e2f54666
mwafrika <37586807+GloireIrenge@users.noreply.github.com>
2022-09-10 22:49:44 +0200

    Create app.js file in src

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { PureComponent } from 'react';
-import { Calculator } from './components/Calculator';
+import Calculator from './components/Calculator';
 import './index.css';
 
 export class App extends PureComponent {
